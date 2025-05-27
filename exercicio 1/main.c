3f93da34e87240a02c584261c62d1ce7b3f546e2
Tiago Gabriel <tiagooliveira.gabriel@gmail.com>
2025-05-27 09:54:07 -0300

    tentando exlcuir gitigonre
    excluir do commit

--- exercicio 1/main.c
+++ exercicio 1/main.c
@@ -1,9 +1,17 @@
 /*
 *ALUNO: Tiago de Oliviera Braga Gabriel RU:5123562
 *DATA DE CRIAÇÃO:27.05.2025
-*REPOSITÓRIO GITHUB:
+*REPOSITÓRIO GITHUB:https://github.com/tobgabriel/liguagemdeprogramacao-uninter
 */
 #include <stdio.h>
+
+//EXIGÊNCIA DE CÓDIGO 1 - Estrutura de Dados: Crie uma struct chamada `Notas`...
+typedef struct{
+    unsigned int notaAPOL1;
+    unsigned int notaAPOL2;
+    unsigned int notaProvaObjetiva;
+    unsigned int notaProvaPratica;
+}NOTAS;
 
 int main(int argc, char *argv[]){
 
